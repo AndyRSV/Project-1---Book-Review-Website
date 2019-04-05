55289f948e1cd36177e686bac5f53eac0263039e
Andy <andysantanavargas@gmail.com>
2019-04-05 13:55:36 -0400

    User registration completed

--- application.py
+++ application.py
@@ -4,7 +4,9 @@ from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flask import render_template
+from flask import render_template, flash, redirect, url_for, logging, request
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 
 app = Flask(__name__)
@@ -28,9 +30,36 @@ def index():
     return render_template('home.html')
 
 
-# @app.route("/login")
-# def index():
-#     return render_template('home.html')
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [
+        validators. DataRequired(),
+        validators.EqualTo('confirm', message='Password do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # insert data in db
+        db.execute(
+            "INSERT INTO users (name, username, password) VALUES (:n, :u, :p)",
+            {"n": name, "u": username, "p": password})
+
+        print("New user created")
+        db.commit()
+
+        flash("You are now registered and can log in!", "success")
+        redirect(url_for('index'))
+
+    return render_template('register.html', form=form)
 
 
 @app.route("/<string:name>")
